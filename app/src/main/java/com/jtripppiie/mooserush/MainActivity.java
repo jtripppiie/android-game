ca0983d3190623d41453262435b7e2dd988bb5e8
jtripppiie <48491097+jtripppiie@users.noreply.github.com>
2026-07-03 18:36:15 -0700

    Enable pause help overlay

--- app/src/main/java/com/jtripppiie/mooserush/MainActivity.java
+++ app/src/main/java/com/jtripppiie/mooserush/MainActivity.java
@@ -44,12 +44,12 @@ public class MainActivity extends Activity {
         );
 
         prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-        gameView = new AlaskaHazardWarningMooseRushView(this);
+        gameView = new AlaskaPauseHelpMooseRushView(this);
         gameView.setPhotoRequestListener(this::openPhotoPicker);
         setContentView(createGameRoot());
         loadSavedPlayerPhoto();
         enableImmersiveMode();
-        Log.d(TAG, "onCreate: Alaska hazard warning game view attached, version badge added, immersive mode enabled");
+        Log.d(TAG, "onCreate: Alaska pause/help game view attached, version badge added, immersive mode enabled");
     }
 
     @Override
