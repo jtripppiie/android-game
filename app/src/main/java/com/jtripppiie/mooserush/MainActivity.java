46e070f01f54c65db5633029fdefb5b6d84e51d9
jtripppiie <48491097+jtripppiie@users.noreply.github.com>
2026-07-03 20:38:18 -0700

    Update game view layer

--- app/src/main/java/com/jtripppiie/mooserush/MainActivity.java
+++ app/src/main/java/com/jtripppiie/mooserush/MainActivity.java
@@ -44,12 +44,12 @@ public class MainActivity extends Activity {
         );
 
         prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-        gameView = new AlaskaProgressMooseRushView(this);
+        gameView = new AlaskaRunSummaryMooseRushView(this);
         gameView.setPhotoRequestListener(this::openPhotoPicker);
         setContentView(createGameRoot());
         loadSavedPlayerPhoto();
         enableImmersiveMode();
-        Log.d(TAG, "onCreate: Alaska progress game view attached, version badge added, immersive mode enabled");
+        Log.d(TAG, "onCreate: Alaska goals game view attached, version badge added, immersive mode enabled");
     }
 
     @Override
