c80cea2e5ffe3f338207d254116e00d11e1c6810
jtripppiie <48491097+jtripppiie@users.noreply.github.com>
2026-07-03 19:13:53 -0700

    Enable bonus pickups layer

--- app/src/main/java/com/jtripppiie/mooserush/MainActivity.java
+++ app/src/main/java/com/jtripppiie/mooserush/MainActivity.java
@@ -44,12 +44,12 @@ public class MainActivity extends Activity {
         );
 
         prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-        gameView = new AlaskaContraCodeMooseRushView(this);
+        gameView = new AlaskaCollectibleMooseRushView(this);
         gameView.setPhotoRequestListener(this::openPhotoPicker);
         setContentView(createGameRoot());
         loadSavedPlayerPhoto();
         enableImmersiveMode();
-        Log.d(TAG, "onCreate: Alaska lives and cheat game view attached, version badge added, immersive mode enabled");
+        Log.d(TAG, "onCreate: Alaska collectible game view attached, version badge added, immersive mode enabled");
     }
 
     @Override
