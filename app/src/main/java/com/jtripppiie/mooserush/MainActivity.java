d0c932cc6a4e9bf118ce3cf35ca6baa0c26a797b
jtripppiie <48491097+jtripppiie@users.noreply.github.com>
2026-07-03 20:22:21 -0700

    Restore immersive layout flag

--- app/src/main/java/com/jtripppiie/mooserush/MainActivity.java
+++ app/src/main/java/com/jtripppiie/mooserush/MainActivity.java
@@ -207,7 +207,7 @@ public class MainActivity extends Activity {
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         );
     }
